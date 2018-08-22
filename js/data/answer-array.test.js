02562fbbe8e07646aa119f4948c77d787f30e975
serrrgun <serrrgun@mail.ru>
2018-08-23 00:06:37 +0300

    Рефакторит тест для getUsersAnswer

--- js/data/answer-array.test.js
+++ js/data/answer-array.test.js
@@ -3,6 +3,6 @@ import {getUsersAnswer} from "./ansewer-array";
 
 describe(`User Answer`, () => {
   it(`must return an array with one object in which the response parameters`, () => {
-    assert.equal(getUsersAnswer(true, 40, 1), [{correct: true, time: 40}]);
+    assert.deepEqual(getUsersAnswer(true, 40, 1), [{correct: true, time: 40}]);
   });
 });
