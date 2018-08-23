5795f31e7a434b0aa53deeac90777d9a41970922
serrrgun <serrrgun@mail.ru>
2018-08-23 14:54:13 +0300

    Добавляет тесты для getUsersAnswer

--- js/data/answer-array.test.js
+++ js/data/answer-array.test.js
@@ -1,8 +1,12 @@
 import {assert} from 'chai';
 import {getUsersAnswer} from "./ansewer-array";
 
-describe(`User Answer`, () => {
+describe(`User answer array`, () => {
   it(`must return an array with one object in which the response parameters`, () => {
     assert.deepEqual(getUsersAnswer(true, 40, 1), [{correct: true, time: 40}]);
+  });
+
+  it(`returns an array with multiple answers`, () => {
+    assert.deepEqual(getUsersAnswer(true, 40, 2), [{correct: true, time: 40}, {correct: true, time: 40}]);
   });
 });
