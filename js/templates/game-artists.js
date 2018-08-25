f25ba6d08d306a1855d48ba89a0f42243d13b54c
serrrgun <serrrgun@mail.ru>
2018-08-25 20:36:12 +0300

    Добавляет header в отдельный шаблон

--- js/templates/game-artists.js
+++ js/templates/game-artists.js
@@ -3,28 +3,12 @@ import {gameResultScreen} from "./result-seccess.js";
 import {gameFailTries} from "./fail-tries.js";
 import {gameFailTime} from "./fail-time.js";
 import welcomeScreen from "./welcome.js";
+import {INITIAL_GAME} from "../data/game";
+import headerTemplate from "./header";
 
 const template = `
 <section class="game game--artist">
-  <header class="game__header">
-    <a class="game__back" href="#">
-      <span class="visually-hidden">Сыграть ещё раз</span>
-      <img class="game__logo" src="img/melody-logo-ginger.png" alt="Угадай мелодию">
-    </a>
-     <svg xmlns="http://www.w3.org/2000/svg" class="timer" viewBox="0 0 780 780">
-      <circle class="timer__line" cx="390" cy="390" r="370" style="filter: url(.#blur); transform: rotate(-90deg) scaleY(-1); transform-origin: center" />
-    </svg>
-     <div class="timer__value" xmlns="http://www.w3.org/1999/xhtml">
-      <span class="timer__mins">05</span>
-      <span class="timer__dots">:</span>
-      <span class="timer__secs">00</span>
-    </div>
-     <div class="game__mistakes">
-      <div class="wrong"></div>
-      <div class="wrong"></div>
-      <div class="wrong"></div>
-    </div>
-  </header>
+  ${headerTemplate(INITIAL_GAME)};
   <section class="game__screen">
     <h2 class="game__title">Кто исполняет эту песню?</h2>
     <div class="game__track">
--- js/templates/game-genre.js
+++ js/templates/game-genre.js
@@ -1,29 +1,12 @@
 import {render, changeScreen} from "../utils.js";
 import {gameArtistScreen} from "./game-artists.js";
 import welcomeScreen from "./welcome.js";
+import {INITIAL_GAME} from "../data/game";
+import headerTemplate from "./header";
 
 const template = `
 <section class="game game--genre">
-  <header class="game__header">
-    <a class="game__back" href="#">
-      <span class="visually-hidden">Сыграть ещё раз</span>
-      <img class="game__logo" src="img/melody-logo-ginger.png" alt="Угадай мелодию">
-    </a>
-     <svg xmlns="http://www.w3.org/2000/svg" class="timer" viewBox="0 0 780 780">
-      <circle class="timer__line" cx="390" cy="390" r="370"
-              style="filter: url(.#blur); transform: rotate(-90deg) scaleY(-1); transform-origin: center"/>
-    </svg>
-     <div class="timer__value" xmlns="http://www.w3.org/1999/xhtml">
-      <span class="timer__mins">05</span>
-      <span class="timer__dots">:</span>
-      <span class="timer__secs">00</span>
-    </div>
-     <div class="game__mistakes">
-      <div class="wrong"></div>
-      <div class="wrong"></div>
-      <div class="wrong"></div>
-    </div>
-  </header>
+   ${headerTemplate(INITIAL_GAME)};
    <section class="game__screen">
     <h2 class="game__title">Выберите инди-рок треки</h2>
     <form class="game__tracks">
