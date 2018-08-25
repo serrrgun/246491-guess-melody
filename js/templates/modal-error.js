f6f3901cf1afeedd95d3f2a70f06144d4b1abfdb
serrrgun <serrrgun@mail.ru>
2018-08-25 17:49:45 +0300

    Добавляет рендер модальных окон

---
+++ js/templates/modal-error.js
@@ -0,0 +1,11 @@
+import {render} from "../utils.js";
+
+const template = `
+<section class="modal">
+  <h2 class="modal__title">Произошла ошибка!</h2>
+  <p class="modal__text">Статус: 404. Пожалуйста, перезагрузите страницу.</p>
+</section>`;
+
+const modalError = render(template);
+
+export {modalError};
