f22cbac04b070b511730377e87b19b582b498302
serrrgun <serrrgun@mail.ru>
2018-08-27 17:03:10 +0300

    Добавляет структуру данных для вопросов

--- js/data/music-data.js
+++ js/data/music-data.js
@@ -1,51 +1,338 @@
 // Music from https://www.youtube.com/audiolibrary/music?feature=blog
-export default [
+export const SONGS = [
   {
-    id: 1,
-    artist: `Kevin MacLeod`,
-    name: `Long Stroll`,
-    image: `https://yt3.ggpht.com/-fkDeGauT7Co/AAAAAAAAAAI/AAAAAAAAAAA/dkF5ZKkrxRo/s900-c-k-no-mo-rj-c0xffffff/photo.jpg`,
-    src: `https://www.youtube.com/audiolibrary_download?vid=91624fdc22fc54ed`,
-    genre: `Jazz`
+    title: `Выберите Jazz`,
+    questions: [
+      {
+        artist: `Kevin MacLeod`,
+        name: `Long Stroll`,
+        image: `https://yt3.ggpht.com/-fkDeGauT7Co/AAAAAAAAAAI/AAAAAAAAAAA/dkF5ZKkrxRo/s900-c-k-no-mo-rj-c0xffffff/photo.jpg`,
+        src: `https://www.youtube.com/audiolibrary_download?vid=91624fdc22fc54ed`,
+        genre: `Jazz`
+      },
+      {
+        artist: `Jingle Punks`,
+        name: `In the Land of Rhinoplasty`,
+        image: `https://i.vimeocdn.com/portrait/992615_300x300`,
+        src: `https://www.youtube.com/audiolibrary_download?vid=dc3b4dc549becd6b`,
+        genre: `Rock`
+      },
+      {
+        artist: `Audionautix`,
+        name: `Travel Light`,
+        image: `http://4.bp.blogspot.com/-kft9qu5ET6U/VPFUBi9W-MI/AAAAAAAACYM/UxXilXKYwOc/s1600/audionautix%2BHalf%2BSize.jpg`,
+        src: `https://www.youtube.com/audiolibrary_download?vid=a127d9b7de8a17cf`,
+        genre: `Country`
+      },
+      {
+        artist: `Jingle Punks`,
+        name: `Lucky Day`,
+        image: `https://i.vimeocdn.com/portrait/992615_300x300`,
+        src: `https://www.youtube.com/audiolibrary_download?vid=bcbe5be936a32fb1`,
+        genre: `Pop`
+      },
+    ],
+    answer: `Jazz`
   },
   {
-    id: 2,
-    artist: `Jingle Punks`,
-    name: `In the Land of Rhinoplasty`,
-    image: `https://i.vimeocdn.com/portrait/992615_300x300`,
-    src: `https://www.youtube.com/audiolibrary_download?vid=dc3b4dc549becd6b`,
-    genre: `Rock`
+    title: `Кто исполняет эту песню?`,
+    question: {
+      artist: `Quincas Moreira`,
+      name: `Firefly`,
+      image: `http://www.atribuna.com.br/fileadmin/_processed_/csm_Quincas-Moreira-Foto-Divulgacao_76d1a8b00e.jpg`,
+      src: `https://www.youtube.com/audiolibrary_download?vid=79100e44c826e2f7`,
+      genre: `Electronic`,
+    },
+    answers: [
+      {
+        artist: `Riot`,
+        name: `Level Plane`,
+        image: `https://i.ytimg.com/vi/jzgM3m8Vp1k/maxresdefault.jpg`,
+        src: `https://www.youtube.com/audiolibrary_download?vid=dfb828f40096184c`,
+        genre: `R&B`,
+      },
+      {
+        artist: `Quincas Moreira`,
+        name: `Firefly`,
+        image: `http://www.atribuna.com.br/fileadmin/_processed_/csm_Quincas-Moreira-Foto-Divulgacao_76d1a8b00e.jpg`,
+        src: `https://www.youtube.com/audiolibrary_download?vid=79100e44c826e2f7`,
+        genre: `Electronic`,
+      },
+      {
+        artist: `Jingle Punks`,
+        name: `Lucky Day`,
+        image: `https://i.vimeocdn.com/portrait/992615_300x300`,
+        src: `https://www.youtube.com/audiolibrary_download?vid=bcbe5be936a32fb1`,
+        genre: `Pop`,
+      },
+    ]
   },
   {
-    id: 3,
-    artist: `Audionautix`,
-    name: `Travel Light`,
-    image: `http://4.bp.blogspot.com/-kft9qu5ET6U/VPFUBi9W-MI/AAAAAAAACYM/UxXilXKYwOc/s1600/audionautix%2BHalf%2BSize.jpg`,
-    src: `https://www.youtube.com/audiolibrary_download?vid=a127d9b7de8a17cf`,
-    genre: `Country`
+    title: `Выберите Rock`,
+    questions: [
+      {
+        artist: `Kevin MacLeod`,
+        name: `Long Stroll`,
+        image: `https://yt3.ggpht.com/-fkDeGauT7Co/AAAAAAAAAAI/AAAAAAAAAAA/dkF5ZKkrxRo/s900-c-k-no-mo-rj-c0xffffff/photo.jpg`,
+        src: `https://www.youtube.com/audiolibrary_download?vid=91624fdc22fc54ed`,
+        genre: `Jazz`
+      },
+      {
+        artist: `Jingle Punks`,
+        name: `In the Land of Rhinoplasty`,
+        image: `https://i.vimeocdn.com/portrait/992615_300x300`,
+        src: `https://www.youtube.com/audiolibrary_download?vid=dc3b4dc549becd6b`,
+        genre: `Rock`
+      },
+      {
+        artist: `Audionautix`,
+        name: `Travel Light`,
+        image: `http://4.bp.blogspot.com/-kft9qu5ET6U/VPFUBi9W-MI/AAAAAAAACYM/UxXilXKYwOc/s1600/audionautix%2BHalf%2BSize.jpg`,
+        src: `https://www.youtube.com/audiolibrary_download?vid=a127d9b7de8a17cf`,
+        genre: `Country`
+      },
+      {
+        artist: `Jingle Punks`,
+        name: `Lucky Day`,
+        image: `https://i.vimeocdn.com/portrait/992615_300x300`,
+        src: `https://www.youtube.com/audiolibrary_download?vid=bcbe5be936a32fb1`,
+        genre: `Pop`
+      },
+    ],
+    answer: `Rock`
   },
   {
-    id: 4,
-    artist: `Riot`,
-    name: `	Level Plane`,
-    image: `https://i.ytimg.com/vi/jzgM3m8Vp1k/maxresdefault.jpg`,
-    src: `https://www.youtube.com/audiolibrary_download?vid=dfb828f40096184c`,
-    genre: `R&B`
+    title: `Кто исполняет эту песню?`,
+    question: {
+      artist: `Riot`,
+      name: `Level Plane`,
+      image: `https://i.ytimg.com/vi/jzgM3m8Vp1k/maxresdefault.jpg`,
+      src: `https://www.youtube.com/audiolibrary_download?vid=dfb828f40096184c`,
+      genre: `R&B`,
+    },
+    answers: [
+      {
+        artist: `Riot`,
+        name: `Level Plane`,
+        image: `https://i.ytimg.com/vi/jzgM3m8Vp1k/maxresdefault.jpg`,
+        src: `https://www.youtube.com/audiolibrary_download?vid=dfb828f40096184c`,
+        genre: `R&B`,
+      },
+      {
+        artist: `Quincas Moreira`,
+        name: `Firefly`,
+        image: `http://www.atribuna.com.br/fileadmin/_processed_/csm_Quincas-Moreira-Foto-Divulgacao_76d1a8b00e.jpg`,
+        src: `https://www.youtube.com/audiolibrary_download?vid=79100e44c826e2f7`,
+        genre: `Electronic`,
+      },
+      {
+        artist: `Jingle Punks`,
+        name: `Lucky Day`,
+        image: `https://i.vimeocdn.com/portrait/992615_300x300`,
+        src: `https://www.youtube.com/audiolibrary_download?vid=bcbe5be936a32fb1`,
+        genre: `Pop`,
+      },
+    ]
   },
   {
-    id: 5,
-    artist: `Jingle Punks`,
-    name: `Lucky Day`,
-    image: `https://i.vimeocdn.com/portrait/992615_300x300`,
-    src: `https://www.youtube.com/audiolibrary_download?vid=bcbe5be936a32fb1`,
-    genre: `Pop`
+    title: `Выберите Country`,
+    questions: [
+      {
+        artist: `Kevin MacLeod`,
+        name: `Long Stroll`,
+        image: `https://yt3.ggpht.com/-fkDeGauT7Co/AAAAAAAAAAI/AAAAAAAAAAA/dkF5ZKkrxRo/s900-c-k-no-mo-rj-c0xffffff/photo.jpg`,
+        src: `https://www.youtube.com/audiolibrary_download?vid=91624fdc22fc54ed`,
+        genre: `Jazz`
+      },
+      {
+        artist: `Jingle Punks`,
+        name: `In the Land of Rhinoplasty`,
+        image: `https://i.vimeocdn.com/portrait/992615_300x300`,
+        src: `https://www.youtube.com/audiolibrary_download?vid=dc3b4dc549becd6b`,
+        genre: `Rock`
+      },
+      {
+        artist: `Audionautix`,
+        name: `Travel Light`,
+        image: `http://4.bp.blogspot.com/-kft9qu5ET6U/VPFUBi9W-MI/AAAAAAAACYM/UxXilXKYwOc/s1600/audionautix%2BHalf%2BSize.jpg`,
+        src: `https://www.youtube.com/audiolibrary_download?vid=a127d9b7de8a17cf`,
+        genre: `Country`
+      },
+      {
+        artist: `Jingle Punks`,
+        name: `Lucky Day`,
+        image: `https://i.vimeocdn.com/portrait/992615_300x300`,
+        src: `https://www.youtube.com/audiolibrary_download?vid=bcbe5be936a32fb1`,
+        genre: `Pop`
+      },
+    ],
+    answer: `Country`
   },
   {
-    id: 6,
-    artist: `Quincas Moreira`,
-    name: `Firefly`,
-    image: `http://www.atribuna.com.br/fileadmin/_processed_/csm_Quincas-Moreira-Foto-Divulgacao_76d1a8b00e.jpg`,
-    src: `https://www.youtube.com/audiolibrary_download?vid=79100e44c826e2f7`,
-    genre: `Electronic`
-  }
+    title: `Кто исполняет эту песню?`,
+    question: {
+      artist: `Riot`,
+      name: `Level Plane`,
+      image: `https://i.ytimg.com/vi/jzgM3m8Vp1k/maxresdefault.jpg`,
+      src: `https://www.youtube.com/audiolibrary_download?vid=dfb828f40096184c`,
+      genre: `R&B`,
+    },
+    answers: [
+      {
+        artist: `Riot`,
+        name: `Level Plane`,
+        image: `https://i.ytimg.com/vi/jzgM3m8Vp1k/maxresdefault.jpg`,
+        src: `https://www.youtube.com/audiolibrary_download?vid=dfb828f40096184c`,
+        genre: `R&B`,
+      },
+      {
+        artist: `Quincas Moreira`,
+        name: `Firefly`,
+        image: `http://www.atribuna.com.br/fileadmin/_processed_/csm_Quincas-Moreira-Foto-Divulgacao_76d1a8b00e.jpg`,
+        src: `https://www.youtube.com/audiolibrary_download?vid=79100e44c826e2f7`,
+        genre: `Electronic`,
+      },
+      {
+        artist: `Jingle Punks`,
+        name: `Lucky Day`,
+        image: `https://i.vimeocdn.com/portrait/992615_300x300`,
+        src: `https://www.youtube.com/audiolibrary_download?vid=bcbe5be936a32fb1`,
+        genre: `Pop`,
+      },
+    ]
+  },
+  {
+    title: `Выберите Pop`,
+    questions: [
+      {
+        artist: `Kevin MacLeod`,
+        name: `Long Stroll`,
+        image: `https://yt3.ggpht.com/-fkDeGauT7Co/AAAAAAAAAAI/AAAAAAAAAAA/dkF5ZKkrxRo/s900-c-k-no-mo-rj-c0xffffff/photo.jpg`,
+        src: `https://www.youtube.com/audiolibrary_download?vid=91624fdc22fc54ed`,
+        genre: `Jazz`
+      },
+      {
+        artist: `Jingle Punks`,
+        name: `In the Land of Rhinoplasty`,
+        image: `https://i.vimeocdn.com/portrait/992615_300x300`,
+        src: `https://www.youtube.com/audiolibrary_download?vid=dc3b4dc549becd6b`,
+        genre: `Rock`
+      },
+      {
+        artist: `Audionautix`,
+        name: `Travel Light`,
+        image: `http://4.bp.blogspot.com/-kft9qu5ET6U/VPFUBi9W-MI/AAAAAAAACYM/UxXilXKYwOc/s1600/audionautix%2BHalf%2BSize.jpg`,
+        src: `https://www.youtube.com/audiolibrary_download?vid=a127d9b7de8a17cf`,
+        genre: `Country`
+      },
+      {
+        artist: `Jingle Punks`,
+        name: `Lucky Day`,
+        image: `https://i.vimeocdn.com/portrait/992615_300x300`,
+        src: `https://www.youtube.com/audiolibrary_download?vid=bcbe5be936a32fb1`,
+        genre: `Pop`
+      },
+    ],
+    answer: `Pop`
+  },
+  {
+    title: `Кто исполняет эту песню?`,
+    question: {
+      artist: `Riot`,
+      name: `Level Plane`,
+      image: `https://i.ytimg.com/vi/jzgM3m8Vp1k/maxresdefault.jpg`,
+      src: `https://www.youtube.com/audiolibrary_download?vid=dfb828f40096184c`,
+      genre: `R&B`,
+    },
+    answers: [
+      {
+        artist: `Riot`,
+        name: `Level Plane`,
+        image: `https://i.ytimg.com/vi/jzgM3m8Vp1k/maxresdefault.jpg`,
+        src: `https://www.youtube.com/audiolibrary_download?vid=dfb828f40096184c`,
+        genre: `R&B`,
+      },
+      {
+        artist: `Quincas Moreira`,
+        name: `Firefly`,
+        image: `http://www.atribuna.com.br/fileadmin/_processed_/csm_Quincas-Moreira-Foto-Divulgacao_76d1a8b00e.jpg`,
+        src: `https://www.youtube.com/audiolibrary_download?vid=79100e44c826e2f7`,
+        genre: `Electronic`,
+      },
+      {
+        artist: `Jingle Punks`,
+        name: `Lucky Day`,
+        image: `https://i.vimeocdn.com/portrait/992615_300x300`,
+        src: `https://www.youtube.com/audiolibrary_download?vid=bcbe5be936a32fb1`,
+        genre: `Pop`,
+      },
+    ]
+  },
+  {
+    title: `Выберите Jazz`,
+    questions: [
+      {
+        artist: `Kevin MacLeod`,
+        name: `Long Stroll`,
+        image: `https://yt3.ggpht.com/-fkDeGauT7Co/AAAAAAAAAAI/AAAAAAAAAAA/dkF5ZKkrxRo/s900-c-k-no-mo-rj-c0xffffff/photo.jpg`,
+        src: `https://www.youtube.com/audiolibrary_download?vid=91624fdc22fc54ed`,
+        genre: `Jazz`
+      },
+      {
+        artist: `Jingle Punks`,
+        name: `In the Land of Rhinoplasty`,
+        image: `https://i.vimeocdn.com/portrait/992615_300x300`,
+        src: `https://www.youtube.com/audiolibrary_download?vid=dc3b4dc549becd6b`,
+        genre: `Rock`
+      },
+      {
+        artist: `Audionautix`,
+        name: `Travel Light`,
+        image: `http://4.bp.blogspot.com/-kft9qu5ET6U/VPFUBi9W-MI/AAAAAAAACYM/UxXilXKYwOc/s1600/audionautix%2BHalf%2BSize.jpg`,
+        src: `https://www.youtube.com/audiolibrary_download?vid=a127d9b7de8a17cf`,
+        genre: `Country`
+      },
+      {
+        artist: `Jingle Punks`,
+        name: `Lucky Day`,
+        image: `https://i.vimeocdn.com/portrait/992615_300x300`,
+        src: `https://www.youtube.com/audiolibrary_download?vid=bcbe5be936a32fb1`,
+        genre: `Pop`
+      },
+    ],
+    answer: `Jazz`
+  },
+  {
+    title: `Кто исполняет эту песню?`,
+    question: {
+      artist: `Riot`,
+      name: `Level Plane`,
+      image: `https://i.ytimg.com/vi/jzgM3m8Vp1k/maxresdefault.jpg`,
+      src: `https://www.youtube.com/audiolibrary_download?vid=dfb828f40096184c`,
+      genre: `R&B`,
+    },
+    answers: [
+      {
+        artist: `Riot`,
+        name: `Level Plane`,
+        image: `https://i.ytimg.com/vi/jzgM3m8Vp1k/maxresdefault.jpg`,
+        src: `https://www.youtube.com/audiolibrary_download?vid=dfb828f40096184c`,
+        genre: `R&B`,
+      },
+      {
+        artist: `Quincas Moreira`,
+        name: `Firefly`,
+        image: `http://www.atribuna.com.br/fileadmin/_processed_/csm_Quincas-Moreira-Foto-Divulgacao_76d1a8b00e.jpg`,
+        src: `https://www.youtube.com/audiolibrary_download?vid=79100e44c826e2f7`,
+        genre: `Electronic`,
+      },
+      {
+        artist: `Jingle Punks`,
+        name: `Lucky Day`,
+        image: `https://i.vimeocdn.com/portrait/992615_300x300`,
+        src: `https://www.youtube.com/audiolibrary_download?vid=bcbe5be936a32fb1`,
+        genre: `Pop`,
+      },
+    ]
+  },
 ];
