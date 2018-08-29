8b54b35a34dc70f9a1a92fc521115833a642704d
serrrgun <serrrgun@mail.ru>
2018-08-29 15:41:45 +0300

    Добавляет склонение существиьельных на экран результатов

--- js/templates/result-seccess.js
+++ js/templates/result-seccess.js
@@ -4,7 +4,7 @@ const template = (points, result, game) => `
   <section class="result">
     <div class="result__logo"><img src="img/melody-logo.png" alt="Угадай мелодию" width="186" height="83"></div>
     <h2 class="result__title">Вы настоящий меломан!</h2>
-    <p class="result__total">За 3 минуты и 25 секунд вы набрали ${points} ${formatWord(points, `point`)}, совершив ${3 - game.lives} ошибки</p>
+    <p class="result__total">За 3 минуты и 25 секунд вы набрали ${points} ${formatWord(points, `point`)}, совершив ${3 - game.lives} ${formatWord(3 - game.lives, `note`)}</p>
     <p class="result__text">${result}</p>
     <button class="result__replay" type="button">Сыграть ещё раз</button>
   </section>`.trim();
