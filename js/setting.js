314b4b6e4c79da83075d65a32878ed4a2da6af93
serrrgun <serrrgun@mail.ru>
2018-09-09 12:54:43 +0300

    Дорабатывает экран выигрыша + правеи по DEBAG

--- js/setting.js
+++ js/setting.js
@@ -1,5 +1,5 @@
 export const DEBAG = true;
-export const DEBUG_STYLE_GENRE = `style="animation: debagAnimation 1.5s ease-out infinite;"`;
+export const DEBUG_STYLE_GENRE = `style="animation: debagAnimation 1s ease-out infinite;"`;
 export const DEBUG_STYLE_ARTIST = `style="color: red; 
                                           font-weight: bold; 
                                           font-size: 20px; 
--- js/view/view--result-secces.js
+++ js/view/view--result-secces.js
@@ -1,6 +1,7 @@
 import View from './view';
 import {FULL_TIME} from "../data/game-data";
 import {formatWord} from "../bisness-logic/declension-word";
+import {getMin, getSec} from "../bisness-logic/change-time";
 
 export default class ViewResultSuccess extends View {
   constructor(model) {
@@ -11,8 +12,8 @@ export default class ViewResultSuccess extends View {
     this.game = model.state;
     this.fullTime = FULL_TIME;
     this._time = this.fullTime - this.game.time;
-    this.min = Math.trunc(this._time / 60);
-    this.sec = (((this._time / 60) - this.min) * 60).toFixed(0);
+    this.min = getMin(this._time);
+    this.sec = getSec(this._time);
   }
 
   get template() {
@@ -20,8 +21,8 @@ export default class ViewResultSuccess extends View {
     <section class="result">
       <div class="result__logo"><img src="img/melody-logo.png" alt="Угадай мелодию" width="186" height="83"></div>
       <h2 class="result__title">Вы настоящий меломан!</h2> 
-      <p class="result__total">За ${this.min > 0 ? `${this.min} ${formatWord(this.sec, `min`)} и` : ``} ${this.sec} ${formatWord(this.sec, `sec`)} вы набрали ${this.points} ${formatWord(this.points, `point`)}, совершив ${3 - this.game.lives} ${formatWord(3 - this.game.lives, `note`)}</p>
-      <p class="result__text">${this.result}</p> 
+      <p class="result__total">За ${this.min}&#160${formatWord(this.min, `min`)} и ${this.sec}&#160${formatWord(this.sec, `sec`)} вы набрали ${this.points} ${formatWord(this.points, `point`)}, совершив ${3 - this.game.lives} ${formatWord(3 - this.game.lives, `note`)}</p>
+      <p class="result__text">${this.result}</p>
       <button class="result__replay" type="button">Сыграть ещё раз</button>
     </section>`.trim();
   }
